b046cf09d2cf2d8de1d9cefabfb721af46ef6674
thehub-com <korada209@gmail.com>
2026-01-15 16:35:27 +0200

    Create firebase.js

---
+++ firebase/firebase.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+
+export const app = initializeApp({
+  apiKey: "AIzaSyDq3tDQnBFXrKW-66JxFPaWG3uso_3XXYY",
+  authDomain: "abs-ai-ec395.firebaseapp.com",
+  projectId: "abs-ai-ec395"
+});
